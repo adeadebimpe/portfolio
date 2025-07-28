f0e9c136d6249ef7a7149fe817cf364fc2a69c3a
Adebimpe Adebowale <adeadebimpe96@gmail.com>
2025-07-28 22:14:21 +0100

    added speed insights and analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "../contexts/ThemeContext";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
     </ThemeProvider>
   );
 }
